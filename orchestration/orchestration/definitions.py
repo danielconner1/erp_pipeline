38d1f23176e890dd9de5edf7c4b3cf34ea6c361b
Daniel Conner <daniel.w.conner@gmail.com>
2026-05-18 19:01:31 -0700

    Removed old dependencies

--- orchestration/orchestration/definitions.py
+++ orchestration/orchestration/definitions.py
@@ -1,7 +1,5 @@
 from dagster import define_asset_job, ScheduleDefinition, Definitions
 from .defs.ingest import ingest_raw_csv_to_parquet
-from .defs.process import process
-from .defs.features import features
 
-defs = Definitions(assets=[ingest_raw_csv_to_parquet, process, features]
+defs = Definitions(assets=[ingest_raw_csv_to_parquet]
                    )
