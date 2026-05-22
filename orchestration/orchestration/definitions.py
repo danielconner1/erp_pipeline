177ae33c5db5a639a93fb92f63b046c23b8eac7e
Daniel Conner <daniel.w.conner@gmail.com>
2026-05-21 21:04:02 -0700

    Removed old definitions

--- orchestration/orchestration/definitions.py
+++
@@ -1,5 +0,0 @@
-from dagster import define_asset_job, ScheduleDefinition, Definitions
-from .defs.ingest import ingest_raw_csv_to_parquet
-
-defs = Definitions(assets=[ingest_raw_csv_to_parquet]
-                   )
